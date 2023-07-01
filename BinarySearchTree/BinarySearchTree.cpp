42ac34584d05bb86e4b76ee1f72acf967393b693
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-07-01 13:44:35 +0700

    variabel class node

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -9,11 +9,4 @@ public:
 	Node* leftchild;
 	Node* rightchild;
 
-	// Constructor for the node class
-	Node(string i, Node* l, Node* r)
-	{
-		info = i;
-		leftchild = l;
-		rightchild = r;
-	}
-};
+}
